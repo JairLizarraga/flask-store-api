96ebbd0a2548859cafb71e07a27d775805d2bf21
Jair Axel Lizarraga Velarde <jairlizarraga@ibm.com>
2024-01-04 13:00:50 -0600

    Base project

---
+++ src/main/java/com/ibm/flaskstoreapi/FlaskStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.flaskstoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlaskStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlaskStoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibm/flaskstoreapi/FlaskStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.flaskstoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlaskStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
