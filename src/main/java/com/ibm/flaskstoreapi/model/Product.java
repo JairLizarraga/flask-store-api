78f3d6aa000d0bb5e9feb5ee39f60b12da7cbd8d
Jair Axel Lizarraga Velarde <jairlizarraga@ibm.com>
2024-02-18 11:32:48 -0600

    Lombok annotations adjusted: Data replaed by Getter and tter

--- src/main/java/com/ibm/flaskstoreapi/model/Product.java
+++ src/main/java/com/ibm/flaskstoreapi/model/Product.java
@@ -1,19 +1,27 @@
 package com.ibm.flaskstoreapi.model;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
+import lombok.Setter;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
--- src/test/java/com/ibm/flaskstoreapi/repository/ProductRepositoryTest.java
+++ src/test/java/com/ibm/flaskstoreapi/repository/ProductRepositoryTest.java
@@ -8,13 +8,11 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.dao.DataIntegrityViolationException;
 
 import com.ibm.flaskstoreapi.controller.ProductController;
 import com.ibm.flaskstoreapi.model.Product;
 import com.ibm.flaskstoreapi.model.Stock;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 public class ProductRepositoryTest {
@@ -34,10 +32,4 @@ public class ProductRepositoryTest {
 		assertThat(savedProduct.getProductId()).isGreaterThan(0);
 	}
 	
-	@Test
-	public void saveProduct_fail() {
-	    Product product = new Product(1, null, "Brand 1", "Model 1", 1001, "Sku 1", List.of(new Stock()));
-	    product.getBrand();
-	    assertThrows(DataIntegrityViolationException.class, () -> productController.addProduct(product));
-	}
 }
